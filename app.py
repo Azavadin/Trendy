8069478d4ffc9bebc67b0358a1425bf8e432861e
Azavadin <scharantejams@gmail.com>
2019-08-18 15:23:04 -0700

    Initial setup

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+link = Flask(__name__)
+link_api = Api(link)
+
+class Link(Resource):
+    def get(self):
+        return {'hello': 'world'}
+    def post(self):
+        params = request.get_json(force=True)
+        user = params.userid
+        url = params.url
+
+link_api.add_resource(Link, '/links')
